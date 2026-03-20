4fc8f7a7a291b52dc87a00a068b03bc2e1cb722c
randy-hx <randy@omnisecurityinc.com>
2026-03-20 17:24:16 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Migrate from './Migrate.jsx'
+import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Migrate />
+    <App />
   </React.StrictMode>
 )
