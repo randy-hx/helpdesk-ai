151a792669884635f76424956ffb6f0dd51ca1fe
randy-hx <randy@omnisecurityinc.com>
2026-03-20 14:18:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'es2020'
+  },
   build: {
     target: 'es2020'
   }
