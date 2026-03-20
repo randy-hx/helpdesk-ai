79f3132cda131d982db83fd960e7904868f1117e
randy-hx <randy@omnisecurityinc.com>
2026-03-20 14:25:44 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    target: 'es2020'
+    target: 'es2022',
+    supported: {
+      'optional-chaining': true,
+      'nullish-coalescing': true,
+    },
   },
   build: {
-    target: 'es2020'
+    target: 'es2022'
   }
 })
